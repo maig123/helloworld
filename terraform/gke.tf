1751d00bde828b7767493a724e3de3cade9f302a
maig123 <maig123@gmail.com>
2024-06-08 17:12:32 -0400

    updates for argocd

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -10,22 +10,68 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
-module "gke" {
-  source  = "terraform-google-modules/kubernetes-engine/google"
-  version = "~> 30.0"
+resource "google_container_cluster" "default" {
+  name               = "gke-standard-zonal-single-zone"
+  location           = "us-east1-b"
+  initial_node_count = 1
 
-  project_id = var.project_id
-  name       = "hellocluster"
-  region     = var.region
-  network    = var.network
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+  network=var.network
   subnetwork = var.subnetwork
+  ip_allocation_policy {
+    cluster_secondary_range_name    = var.pod_range
+    services_secondary_range_name = var.service_range
+  }
 
-  ip_range_pods          = var.pod_range
-  ip_range_services      = var.service_range
-  create_service_account = true
+  # create_service_account = true
   # service_account        = var.compute_engine_service_account
-  deletion_protection    = false
+
   default_max_pods_per_node = 50
 
-  depends_on = [google_compute_subnetwork.hellosubnet, google_compute_network.hellovpc]
+
+    node_config {
+        boot_disk_kms_key           = null
+        disk_size_gb                = 40
+        disk_type                   = "pd-standard"
+
+        enable_confidential_storage = false
+        guest_accelerator           = []
+        image_type                  = "COS_CONTAINERD"
+        labels                      = {
+            "cluster_name" = "hellocluster"
+            "node_pool"    = "n1-pool"
+        }
+        local_ssd_count             = 0
+        logging_variant             = "DEFAULT"
+        machine_type                = "e2-standard-4"
+        metadata                    = {
+            "cluster_name"             = "hellocluster"
+            "disable-legacy-endpoints" = "true"
+            "node_pool"                = "n1-pool"
+        }
+        min_cpu_platform            = null
+        node_group                  = null
+        oauth_scopes                = [
+            "https://www.googleapis.com/auth/cloud-platform",
+        ]
+        preemptible                 = false
+        resource_labels             = {}
+        resource_manager_tags       = {}
+
+        spot                        = true
+        tags                        = [
+            "gke-hellocluster",
+            "gke-hellocluster-n1-pool",
+        ]
+
+        shielded_instance_config {
+            enable_integrity_monitoring = true
+            enable_secure_boot          = false
+        }
+
+    }
+
+depends_on = [google_compute_subnetwork.hellosubnet, google_compute_network.hellovpc]
 }
